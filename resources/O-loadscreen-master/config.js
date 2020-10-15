429345a831ab281db96fe558ae61f8d3ad627e54
chetan177 <chetanpandey177@gmail.com>
2020-10-15 20:33:56 +0530

    resources added

---
+++ resources/O-loadscreen-master/config.js
@@ -0,0 +1,16 @@
+var config = {
+    /* In milliseconds */
+    "transitionInterval": 1250,
+    "imgInterval": 8000,
+    "music": true,
+    /* Insert the Youtube Video ID below */
+    "videoID": "Er2L0Yi2okE",
+    /* Music Volume (0%-100%) | Lower is Quieter, Higher is Louder */
+    "musicVolume": 20,
+    "text": {
+        "title": "OMEGA Roleplay Test Server",
+        "link": "discord.gg/UmgeKnh",
+    },
+    /* You dont have to include the '/imgs/' dir AND ALSO REMEMBER TO ADD THESE IMGS IN YOUR __resource.lua*/ 
+    "images": ['bg0.png', 'bg1.png', 'bg2.png', 'bg3.png', 'bg4.png', 'bg6.png']
+}
---
+++ resources/[gamemodes]/[maps]/fivem-map-skater/map.lua
@@ -0,0 +1,16 @@
+ --When editing this file, make sure to have at least 2 Spawnpoints!
+
+
+
+
+
+
+
+
+
+
+spawnpoint 'a_m_y_skater_01' { x = -802.311, y = 175.056, z = 72.8446 }
+spawnpoint 'a_m_y_skater_02' { x = -9.96562, y = -1438.54, z = 31.1015 }
+
+
+--
